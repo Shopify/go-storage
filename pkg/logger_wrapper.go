38563ce8b01331f22b84f019e409d26a15073af3
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2018-11-07 20:17:28 -0500

    Use interface for logger wrapper

--- pkg/logger_wrapper.go
+++ pkg/logger_wrapper.go
@@ -2,12 +2,17 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"io"
-	"log"
 )
 
+// Logger can be a *log.Logger
+type Logger interface {
+	Print(v ...interface{})
+}
+
 // NewLoggerWrapper creates a new FS which logs all calls to FS.
-func NewLoggerWrapper(fs FS, name string, l *log.Logger) FS {
+func NewLoggerWrapper(fs FS, name string, l Logger) FS {
 	return &loggerWrapper{
 		fs:     fs,
 		name:   name,
@@ -20,35 +25,39 @@ type loggerWrapper struct {
 	fs FS
 
 	name   string
-	logger *log.Logger
+	logger Logger
+}
+
+func (l *loggerWrapper) printf(format string, v ...interface{}) {
+	l.logger.Print(fmt.Sprintf(format, v...))
 }
 
 // Open implements FS.  All calls to Open are logged and errors are logged separately.
 func (l *loggerWrapper) Open(ctx context.Context, path string) (*File, error) {
-	l.logger.Printf("%v: open: %v", l.name, path)
+	l.printf("%v: open: %v", l.name, path)
 	f, err := l.fs.Open(ctx, path)
 	if err != nil {
-		l.logger.Printf("%v: open error: %v: %v", l.name, path, err)
+		l.printf("%v: open error: %v: %v", l.name, path, err)
 	}
 	return f, err
 }
 
 // Create implements FS.  All calls to Create are logged and errors are logged separately.
 func (l *loggerWrapper) Create(ctx context.Context, path string) (io.WriteCloser, error) {
-	l.logger.Printf("%v: create: %v", l.name, path)
+	l.printf("%v: create: %v", l.name, path)
 	wc, err := l.fs.Create(ctx, path)
 	if err != nil {
-		l.logger.Printf("%v: create error: %v: %v", l.name, path, err)
+		l.printf("%v: create error: %v: %v", l.name, path, err)
 	}
 	return wc, err
 }
 
 // Delete implements FS.  All calls to Delete are logged and errors are logged separately.
 func (l *loggerWrapper) Delete(ctx context.Context, path string) error {
-	l.logger.Printf("%v: delete: %v", l.name, path)
+	l.printf("%v: delete: %v", l.name, path)
 	err := l.fs.Delete(ctx, path)
 	if err != nil {
-		l.logger.Printf("%v: delete error: %v: %v", l.name, path, err)
+		l.printf("%v: delete error: %v: %v", l.name, path, err)
 	}
 	return err
 }
@@ -59,10 +68,10 @@ func (l *loggerWrapper) Walk(ctx context.Context, path string, fn WalkFn) error 
 }
 
 func (l *loggerWrapper) URL(ctx context.Context, path string, options *SignedURLOptions) (string, error) {
-	l.logger.Printf("%v: URL: %v", l.name, path)
+	l.printf("%v: URL: %v", l.name, path)
 	url, err := l.fs.URL(ctx, path, options)
 	if err != nil {
-		l.logger.Printf("%v: URL error: %v: %v", l.name, path, err)
+		l.printf("%v: URL error: %v: %v", l.name, path, err)
 	}
 	return url, err
 
