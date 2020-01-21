b1aa46b28aa7a4fa9c5f3e565c20f5c7238a1175
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2020-01-21 15:44:26 -0500

    Add utility functions
    Add `Exists`, `Read`, and `Write`, which are common operations.

---
+++ pkg/utils.go
@@ -0,0 +1,47 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+func Read(ctx context.Context, fs FS, path string, options *ReaderOptions) (data []byte, err error) {
+	var file *File
+	if file, err = fs.Open(ctx, path, options); err != nil {
+		return nil, fmt.Errorf("unable to open %s: %w", path, err)
+	}
+
+	if data, err = ioutil.ReadAll(file); err != nil {
+		return nil, fmt.Errorf("unable to read %s: %w", path, err)
+	}
+
+	if err = file.Close(); err != nil {
+		return data, fmt.Errorf("unable to close %s: %w", path, err)
+	}
+
+	return data, nil
+}
+
+func Write(ctx context.Context, fs FS, path string, data []byte, options *WriterOptions) (err error) {
+	var w io.WriteCloser
+	if w, err = fs.Create(ctx, path, options); err != nil {
+		return fmt.Errorf("unable to create %s: %w", path, err)
+	}
+
+	if _, err = w.Write(data); err != nil {
+		return fmt.Errorf("unable to write %s: %w", path, err)
+	}
+
+	if err = w.Close(); err != nil {
+		return fmt.Errorf("unable to close %s: %w", path, err)
+	}
+
+	return nil
+}
+
+func Exists(ctx context.Context, fs FS, path string) bool {
+	attrs, _ := fs.Attributes(ctx, path, nil)
+	return attrs != nil
+}
