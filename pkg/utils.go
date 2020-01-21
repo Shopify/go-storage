2d1109791b2c71221d2a4e71029a98d2c83a125a
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2020-01-21 18:27:00 -0500

    Close before returning on error

--- pkg/utils.go
+++ pkg/utils.go
@@ -17,6 +17,7 @@ func Read(ctx context.Context, fs FS, path string, options *ReaderOptions) ([]by
 
 	var data []byte
 	if data, err = ioutil.ReadAll(file); err != nil {
+		_ = file.Close() // Best effort at cleaning up
 		return nil, fmt.Errorf("unable to read %s: %w", path, err)
 	}
 
@@ -36,6 +37,7 @@ func Write(ctx context.Context, fs FS, path string, data []byte, options *Writer
 	}
 
 	if _, err = w.Write(data); err != nil {
+		_ = w.Close() // Best effort at cleaning up
 		return fmt.Errorf("unable to write %s: %w", path, err)
 	}
 
